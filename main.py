3bb4b02f845410db5d5dcad9d07efb8e6fdcaa1c
Abysxs <rubenbiz27@gmail.com>
2024-08-29 17:41:10 -0500

    made changes to open_reddit function

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.edge.service import Service
 from selenium.webdriver.edge.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
+
 def main():
-    url = "https://www.reddit.com"
+    url = "https://www.reddit.com/login/"
     open_reddit(url)
 
 def open_reddit(url):
@@ -22,9 +25,26 @@ def open_reddit(url):
     driver = webdriver.Edge(service=service, options=edge_options)
 
     driver.get(url)
+
+    # This will wait until these fields are visible
+    wait = WebDriverWait(driver, 5)
+    usernameField = wait.until(EC.presence_of_element_located((By.ID, "login-username")))
+    passwordField = wait.until(EC.presence_of_element_located((By.ID, "login-password")))
+
+    time.sleep(5)
+
+    # User inputs username and password
+    usernameInput = input("Username: ")
+    passwordInput = input("Password: ")
+
+    # Sends the user's inputs into those field
+    usernameField.send_keys(usernameInput)
+    passwordField.send_keys(passwordInput)
+
     # Keep the browser open until manually closed
     input("Press Enter to exit and close the browser...")
     driver.quit()
+
 
 if __name__ == "__main__":
     main()
